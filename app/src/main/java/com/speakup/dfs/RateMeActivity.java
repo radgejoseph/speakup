3b6956c8bafcc80383bfc54b8f6a13b2373cc114
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2020-10-18 17:38:50 +0800

    All layouts are working , minor resizing  only of items, tab layout is now clickable. added rating function

--- app/src/main/java/com/speakup/dfs/RateMeActivity.java
+++ app/src/main/java/com/speakup/dfs/RateMeActivity.java
@@ -10,15 +10,23 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class RateMeActivity extends AppCompatActivity {
 
     private TextView textPlate;
+    TextView rateCount;
+    EditText review;
     Toolbar toolbar;
+    RatingBar ratingBar;
+    Button submit_button;
+    int rateValue; String temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,11 @@ public class RateMeActivity extends AppCompatActivity {
         });
 
 
+        ratingBar = findViewById(R.id.ratingBar);
+        review = findViewById(R.id.review_box);
+        submit_button = findViewById(R.id.submit_button);
+        rateCount = findViewById(R.id.ratecount);
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -45,6 +58,35 @@ public class RateMeActivity extends AppCompatActivity {
         String plate = getIntent().getStringExtra("selected_plate");
         textPlate = findViewById(R.id.plate_number);
         textPlate.setText(plate);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                rateValue = (int) ratingBar.getRating();
+
+                if (rateValue <= 1 && rateValue > 0)
+                    rateCount.setText("Bad " +rateValue+ "/5");
+                else if (rateValue <= 2 && rateValue > 1)
+                    rateCount.setText("OK " +rateValue+ "/5");
+                else if (rateValue <= 3 && rateValue > 2)
+                    rateCount.setText("Good " +rateValue+ "/5");
+                else if (rateValue <= 4 && rateValue > 3)
+                    rateCount.setText("Very Good " +rateValue+ "/5");
+                else if (rateValue <= 5 && rateValue > 4)
+                    rateCount.setText("Best " +rateValue+ "/5");
+            }
+        });
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                temp = review.getText().toString();
+                Toast.makeText(RateMeActivity.this, "Your Rating:\n" + rateValue + "\n" + temp, Toast.LENGTH_SHORT).show();
+                review.setText("");
+                ratingBar.setRating(0);
+                rateCount.setText("");
+            }
+        });
 
 
     }
