87adda56efd762ad36abb19bbbe429b786d90e64
radgejoseph <radgejoseph.sanjoaquin@gmail.com>
2021-07-24 13:26:20 +0800

    *** July 24, 2021 Remove unused Imports Again***

--- app/src/main/java/com/speakup/dfs/PlateRatingsActivityView.java
+++ app/src/main/java/com/speakup/dfs/PlateRatingsActivityView.java
@@ -11,8 +11,6 @@ import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
--- app/src/main/java/com/speakup/dfs/RegisterComplete.java
+++ app/src/main/java/com/speakup/dfs/RegisterComplete.java
@@ -3,7 +3,6 @@ package com.speakup.dfs;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class RegisterComplete extends AppCompatActivity {
